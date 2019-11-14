38cbba47a6050c5d035f11bf5ada09fb2f3fa876
Huseyn <ashurlu85@gmail.com>
2019-11-14 22:14:02 +0400

    Card 2

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+'use strict';
 // $(".mybtn").click(function(){
 //     $(this).parents(".parents").find("span").css("color","red")
 
@@ -20,38 +21,83 @@
 //     this.parentNode.parentNode
 // }
 
-const btnDom=$("[data-btn='Add_To_Cart']")
-btnDom.click(function(){
-    
-  const cardParent= $(this).parents(".card")
-  const product={
-      image:cardParent.find(".card-img-top").attr("src"),
-      name:cardParent.find(".card-title").text(),
-      price:cardParent.find(".card-price").text(),
-      quantity:1
+const btnDom = $("[data-btn='Add_To_Cart']")
+
+
+let card = []
+
+btnDom.click(function () {
+
+
+
+
+    const cardParent = $(this).parents(".card")
+    const product = {
+        image: cardParent.find(".card-img-top").attr("src"),
+        name: cardParent.find(".card-title").text(),
+        price: cardParent.find(".card-price").text(),
+        quantity: 1
     }
-    const modal=document.querySelector(".modal-body")
-    
-    modal.insertAdjacentHTML("beforeEnd",`
-    <div class="md-Cart d-flex justify-content-between">
-    <div class="col-lg-2">
-        <img src="${product.image}" class="img-fluid" alt="">
-    </div>
-    <div class="col-lg-5">
-        <p>${product.name}</p>
-    </div>
-    <div>
-        <span class="md-price">${product.price}</span>
-        <button class="btn btn-sm btn-danger">-</button>
-        <span>${product.quantity}</span>
-        <button class="btn btn-sm btn-primary">+</button>
+    const modal = document.querySelector(".modal-body")
 
-    </div>
-</div>
+
+    let isInCard = card.filter(cardItem => (cardItem.name.trim() === product.name.trim())).length > 0;
+    if (!isInCard) {
+        modal.insertAdjacentHTML("beforeEnd", `
+        <div class="md-card d-flex justify-content-between">
+        <div class="col-lg-2">
+            <img src="${product.image}" class="img-fluid" alt="">
+        </div>
+        <div class="col-lg-5">
+            <p class="card-name">${product.name}</p>
+        </div>
+        <div>
+            <span class="md-price">${product.price}</span>
+            <button data-btn="Decrease_Item" class="btn btn-sm btn-danger">-</button>
+            <span class="card-quantity">${product.quantity}</span>
+            <button data-btn="Increase_Item" class="btn btn-sm btn-primary">+</button>
+            <button data-btn="Remove_Item" class="btn btn-sm btn-danger">&times;</button>
     
-    `)
+        </div>
+    </div>
+        
+        `)
+    }
+    card.push(product)
+    const mdCard = $(".md-card")
+    mdCard.each(function (vaqif, kamran) {
+        let cardText = $(kamran).find(".card-name").text()
+        if (cardText === product.name) {
+            $(kamran).find("[data-btn='Increase_Item']").click(function () {
+                card.forEach(cardItem => {
+                    if (cardItem.name == product.name) {
+                       let newPrice= Number(cardItem.price.split("Azn")[0]);
+                       $(kamran).find(".card-quantity").text(++cardItem.quantity)
+                       $(kamran).find(".md-price").text(newPrice*cardItem.quantity+" Azn")
+                    }
+                })
+            })
+            $(kamran).find("[data-btn='Decrease_Item']").click(function () {
+                card.forEach(cardItem => {
+                    if (cardItem.name == product.name) {
+                        if (cardItem.quantity > 1) {
+                            let newPrice= Number(cardItem.price.split("Azn")[0]);
+                            $(kamran).find(".card-quantity").text(--cardItem.quantity)
+                            $(kamran).find(".md-price").text(newPrice*cardItem.quantity+" Azn")
+
+                        }
+                    }
+                })
+            })
+            $(kamran).find("[data-btn='Remove_Item']").click(function () {
+                card.forEach(cardItem => {
+                        $(kamran).remove()
+                        console.log(card)
+                })
+            })
+        }
+    })
 })
-
 
 
 
